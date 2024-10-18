8274b58f2597a263622756a91839c2027102e0e6
ryotaro-130 <t236032@st.yamagata-u.ac.jp>
2024-10-18 19:24:45 +0900

    Add files via upload

---
+++ kadai4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int k,m;
+    printf("How many novels?");
+    scanf("%d",&k);
+    
+
+    printf("How many comics?");
+    scanf("%d",&m);
+    
+    printf("The number of books is %d", k+m);
+
+    return 0;
+    
+}
