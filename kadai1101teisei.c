bc6192854ff856cb6a7e710716b5e4f02dc79037
ryotaro-130 <t236032@st.yamagata-u.ac.jp>
2024-11-01 19:23:48 +0900

    Add files via upload

---
+++ kadai1101teisei.c
@@ -0,0 +1,15 @@
+#include <float.h>
+#include <limits.h>
+#include <stdio.h>
+
+int main(void)
+{
+    printf("float型の最大値:%g\n", FLT_MAX);
+    printf("float型の最小値:%g\n", FLT_MIN);
+    printf("float型の符号なし最大値:%d\n", USHRT_MAX);
+    printf("short型の最大値:%d\n", SHRT_MAX);
+    printf("short型の最小値:%d\n", SHRT_MIN);
+    
+    
+    return 0;
+}
